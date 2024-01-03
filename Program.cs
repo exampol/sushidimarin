39e607c4d44ac47264429f4d23120f53df8fb3a1
Andrea Marin <marinandreeaa@gmail.com>
2024-01-03 13:04:09 +0100

    0.0.0.0

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 var port = Environment.GetEnvironmentVariable("PORT");
 
 //set listening urls
-builder.WebHost.UseUrls($"http://localhost:3000");
+builder.WebHost.UseUrls($"http://0.0.0.0:3000");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
